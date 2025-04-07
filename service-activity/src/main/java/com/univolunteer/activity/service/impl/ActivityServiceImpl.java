c04ceab40fa2da76b9d9118eb75b0b575a20ea31
MYX24 <3066902089@qq.com>
2025-04-07 08:52:17 +0800

    fix:修改日志模块

--- service-activity/src/main/java/com/univolunteer/activity/service/impl/ActivityServiceImpl.java
+++ service-activity/src/main/java/com/univolunteer/activity/service/impl/ActivityServiceImpl.java
@@ -401,19 +401,21 @@ public class ActivityServiceImpl extends ServiceImpl<ActivityMapper, Activity> i
         if (StringUtils.isNotBlank(category)){
             queryWrapper.eq("category", category);
         }
-        // 首先解析为 LocalDateTime（完整的日期时间格式）
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime parsedTime = LocalDateTime.parse(time, dateTimeFormatter);
-        LocalDateTime dayStart = parsedTime.toLocalDate().atStartOfDay(); // 2025-04-10 00:00:00
-        LocalDateTime dayEnd = parsedTime.toLocalDate().atTime(23, 59, 59, 999999999);
-        queryWrapper.between("start_time", dayStart, dayEnd);
+        if (StringUtils.isNotBlank(time)){
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime parsedTime = LocalDateTime.parse(time, dateTimeFormatter);
+            LocalDateTime dayStart = parsedTime.toLocalDate().atStartOfDay(); // 2025-04-10 00:00:00
+            LocalDateTime dayEnd = parsedTime.toLocalDate().atTime(23, 59, 59, 999999999);
+            queryWrapper.between("start_time", dayStart, dayEnd);
+        }
         if (UserContext.get().getRole()== UserRoleEnum.VOLUNTEER){
             queryWrapper.eq("status", 1);
         }
         if (StringUtils.isNotBlank(location)){
             queryWrapper.like("location", location);
         }
-        IPage<ActivityVO> allActivityVO = getAllActivityVO(page, size, activityPage);
+        Page<Activity> activities = this.page(activityPage, queryWrapper);
+        IPage<ActivityVO> allActivityVO = getAllActivityVO(page, size, activities);
         return Result.ok(allActivityVO.getRecords(), allActivityVO.getTotal());
     }
 
